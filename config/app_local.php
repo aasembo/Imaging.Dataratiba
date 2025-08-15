bdfa4b112cdb8b25dd054c404345f791271ac10a
arpit-rockers <arpitkhandelwal86@gmail.com>
2025-08-15 23:23:41 +0530

    update the code

--- config/app_local.php
+++ config/app_local.php
@@ -47,7 +47,7 @@ return [
             'username' => 'root',
             'password' => '',
 
-            'database' => 'dcmc',
+            'database' => 'DemoImaging',
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
